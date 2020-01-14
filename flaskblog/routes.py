0f37f023a1a2938b02b0e9a8d0b37ba71740e209
Kate Kate <snoblomma@gmail.com>
2020-01-14 07:30:57 +0000

    reset token template

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -146,5 +146,17 @@ def reset_request():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
     return render_template('reset_request.html', title='Reset Password', form=form)
 
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token!', 'warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    return render_template('reset_token.html', title='Reset Password', form=form)
